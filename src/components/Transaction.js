52de9f7ffbc9471ae9f083f0ab01e8c8da36857a
github username <github email address>
2017-06-23 11:27:45 -0400

    done

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,14 +1,19 @@
 import React from 'react'
 
-const Transaction = () => {
+const Transaction = (props) => {
   return (
     <tr>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
+      <td>{props.trans.posted_at}</td>
+      <td>{props.trans.description}</td>
+      <td>{props.trans.category}</td>
+      <td>{props.trans.amount}</td>
     </tr>
   )
 }
+
+//           posted_at: "2017-05-24 08:52:00",
+          // description: "Medium Iced Cold Brew, Gregory's Coffee",
+          // category: "Food",
+          // amount: -365
 
 export default Transaction
