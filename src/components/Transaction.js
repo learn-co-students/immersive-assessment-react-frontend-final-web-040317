86b07c50f3fa32495b2f931f1a1c8d479cf29dc8
Ryan Ferguson <fergusman@gmail.com>
2017-06-23 10:22:00 -0400

    finished lil challenge

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-const Transaction = () => {
+const Transaction = (props) => {
+  
   return (
-    <tr>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
+    <tr key={props.item.id}>
+      <td>{props.item.posted_at}</td>
+      <td>{props.item.description}</td>
+      <td>{props.item.category}</td>
+      <td>{props.item.amount}</td>
     </tr>
   )
 }
